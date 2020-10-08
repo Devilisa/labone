53d0b2a85b46ca293eae1cbaba56ede8c4507a7a
Devilisa <betskovase@bk.ru>
2020-10-08 21:35:02 +0300

    Учла все недочеты

--- newchanged.py
+++ newchanged.py
@@ -1,33 +1,33 @@
 with open(r'C:\Users\HOME\Desktop\labone\steam_description_data.csv', encoding='utf-8') as f:
-    k1 = 0
+    sym_num = 0
 # общее количество символов
-    k2 = 0
+    space_num = 0
 # количество пробелов
-    k3 = 0
+    letters_num = 0
 # количество букв и цифр
-    k4 = 0
+    words_num = 0
 # количество слов при условии, что слово это последовательность символов до ".", ",", "!", "?" или " "
-    k5 = 0
+    sentences_num = 0
 # количество предложений
     s = ''
     p = ''
-    for i in f:
-        for j in i:
-            k1 += 1
+    for line in f:
+        for j in line:
+            sym_num += 1
             if j == ' ':
-                k2 += 1
-            if j.isalnum() == 1:
-                k3 += 1
-            if s.isalnum() == 1 and (j == ' ' or j == '.' or j == ',' or j == '!' or j == '?'):
-                k4 += 1
+                space_num += 1
+            if j.isalnum():
+                letters_num += 1
+            if s.isalnum() and j in ',.!? ':
+                words_num += 1
             s = j
-            if j != '.' and j != '!' and j != '?':
+            if j not in '!?.':
                 p += j
             elif p != '':
-                k5 += 1
+                sentences_num += 1
                 p = ''
-print(k1)
-print(k1 - k2)
-print(k3 + k2)
-print(k4)
-print(k5)
+print('Количество символов всего: ', sym_num)
+print('Количество символов без пробелов: ', sym_num - space_num)
+print('Количество символов без знаков препинания: ', letters_num + space_num)
+print('Количество слов: ', words_num)
+print('Количество предложений: ', sentences_num)
